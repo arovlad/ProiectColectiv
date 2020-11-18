334b4fabeaefccf6cdf8a7597a96db5a51792468
mihai.krisan <mihai.krisan@yahoo.com>
2020-11-18 18:46:49 +0200

    exceptions refactor

--- EmployeeManagementApp/EmployeeAppPersistence/src/main/java/ro/ubb/implementations/SkillDaoImpl.java
+++ EmployeeManagementApp/EmployeeAppPersistence/src/main/java/ro/ubb/implementations/SkillDaoImpl.java
@@ -1,6 +1,7 @@
 package ro.ubb.implementations;
 
 import ro.ubb.constants.TechnologyArea;
+import ro.ubb.exceptions.DbException;
 import ro.ubb.interfaces.GenericDao;
 import ro.ubb.interfaces.SkillDao;
 import ro.ubb.models.Skill;
@@ -11,100 +12,136 @@ import java.sql.*;
 public class SkillDaoImpl implements GenericDao, SkillDao {
 
     @Override
-    public Skill find(int id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        DatabaseConnection databaseConnection = new DatabaseConnection();
-        Connection connection = databaseConnection.getConnection();
+    public Skill find(int id) throws DbException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        String querySelect = "SELECT * FROM skills WHERE ID = " + id;
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(querySelect);
+        try {
+            DatabaseConnection databaseConnection = new DatabaseConnection();
+            Connection connection = databaseConnection.getConnection();
 
-        int skillID;
-        String skillName;
-        String technologyAreaName;
-        TechnologyArea technologyArea = null;
+            String querySelect = "SELECT * FROM skills WHERE ID = " + id;
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(querySelect);
 
-        if (resultSet.next()) {
-            skillID = resultSet.getInt("ID");
-            skillName = resultSet.getString("Skill_Name");
+            int skillID;
+            String skillName;
+            String technologyAreaName;
+            TechnologyArea technologyArea = null;
 
-            technologyAreaName = resultSet.getString("Technologies_Area");
-            TechnologyArea[] technologyAreas = TechnologyArea.values();
-            for (TechnologyArea area : technologyAreas) {
-                if (area.getTechnologyArea().equals(technologyAreaName)) {
-                    technologyArea = area;
+            if (resultSet.next()) {
+                skillID = resultSet.getInt("ID");
+                skillName = resultSet.getString("Skill_Name");
+
+                technologyAreaName = resultSet.getString("Technologies_Area");
+                TechnologyArea[] technologyAreas = TechnologyArea.values();
+                for (TechnologyArea area : technologyAreas) {
+                    if (area.getTechnologyArea().equals(technologyAreaName)) {
+                        technologyArea = area;
+                    }
                 }
-            }
-        } else
-            return null;
+            } else
+                return null;
 
-        Skill skill = new Skill();
-        skill.setId(skillID);
-        skill.setSkillName(skillName);
-        skill.setTechnologyArea(technologyArea);
+            Skill skill = new Skill();
+            skill.setId(skillID);
+            skill.setSkillName(skillName);
+            skill.setTechnologyArea(technologyArea);
 
-        return skill;
+            return skill;
+        } catch (SQLException sqlException) {
+            throw new DbException("Something went wrong with the database");
+        }
     }
 
     @Override
-    public Skill save(Object entity) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        DatabaseConnection databaseConnection = new DatabaseConnection();
-        Connection connection = databaseConnection.getConnection();
+    public Skill save(Object entity) throws DbException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        Skill skill = (Skill) entity;
+        try {
+            DatabaseConnection databaseConnection = new DatabaseConnection();
+            Connection connection = databaseConnection.getConnection();
 
-        String queryInsert = "INSERT INTO skills(Skill_Name,Technologies_Area) " +
-                "VALUES (?,?);";
-        PreparedStatement statement = connection.prepareStatement(queryInsert);
-        statement.setString(1, skill.getSkillName());
-        statement.setString(2, skill.getTechnologyArea().getTechnologyArea());
-        int result = statement.executeUpdate();
+            Skill skill = (Skill) entity;
 
-        if (result == 1)
-            return skill;
-        else
-            return null;
+            String queryInsert = "INSERT INTO skills(Skill_Name,Technologies_Area) " +
+                    "VALUES (?,?);";
+            PreparedStatement statement = connection.prepareStatement(queryInsert);
+            statement.setString(1, skill.getSkillName());
+            statement.setString(2, skill.getTechnologyArea().getTechnologyArea());
+            int result = statement.executeUpdate();
+
+            if (result == 1)
+                return skill;
+            else
+                return null;
+        } catch (SQLException sqlException) {
+            throw new DbException("Something went wrong with the database");
+        }
     }
 
     @Override
-    public Skill delete(int id) throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        DatabaseConnection databaseConnection = new DatabaseConnection();
-        Connection connection = databaseConnection.getConnection();
+    public Skill delete(int id) throws DbException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        String queryDelete = "DELETE FROM skills WHERE ID = ?";
-        PreparedStatement preparedStatement = connection.prepareStatement(queryDelete);
-        preparedStatement.setInt(1, id);
-        Skill skill = find(id);
-        int result = preparedStatement.executeUpdate();
+        try {
+            DatabaseConnection databaseConnection = new DatabaseConnection();
+            Connection connection = databaseConnection.getConnection();
 
-        if (result == 1)
-            return skill;
-        else
-            return null;
+            String queryDelete = "DELETE FROM skills WHERE ID = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(queryDelete);
+            preparedStatement.setInt(1, id);
+            Skill skill = find(id);
+            int result = preparedStatement.executeUpdate();
+
+            if (result == 1)
+                return skill;
+            else
+                return null;
+        } catch (SQLException sqlException) {
+            throw new DbException("Something went wrong with the database");
+        }
     }
 
     @Override
-    public Skill update(Object entity) throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        DatabaseConnection databaseConnection = new DatabaseConnection();
-        Connection connection = databaseConnection.getConnection();
+    public Skill update(Object entity) throws DbException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        Skill skill = (Skill) entity;
+        try {
+            DatabaseConnection databaseConnection = new DatabaseConnection();
+            Connection connection = databaseConnection.getConnection();
 
-        String queryUpdate = "UPDATE Skills SET Skill_Name = ?, " +
-                "Technologies_Area = ? WHERE ID = ?";
-        PreparedStatement statement = connection.prepareStatement(queryUpdate);
-        statement.setString(1, skill.getSkillName());
-        statement.setString(2, skill.getTechnologyArea().getTechnologyArea());
-        statement.setInt(3, skill.getId());
-        int result = statement.executeUpdate();
+            Skill skill = (Skill) entity;
 
-        if (result == 1)
-            return skill;
-        else
-            return null;
+            String queryUpdate = "UPDATE Skills SET Skill_Name = ?, " +
+                    "Technologies_Area = ? WHERE ID = ?";
+            PreparedStatement statement = connection.prepareStatement(queryUpdate);
+            statement.setString(1, skill.getSkillName());
+            statement.setString(2, skill.getTechnologyArea().getTechnologyArea());
+            statement.setInt(3, skill.getId());
+            int result = statement.executeUpdate();
+
+            if (result == 1)
+                return skill;
+            else
+                return null;
+        } catch (SQLException sqlException) {
+            throw new DbException("Something went wrong with the database");
+        }
     }
 }
