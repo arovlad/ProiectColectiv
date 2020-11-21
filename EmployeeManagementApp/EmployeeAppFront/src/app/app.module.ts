a8acc394445a0b22ca4f5fb4f06f81614043ab6c
andrada-code <65818802+andrada-code@users.noreply.github.com>
2020-11-21 23:24:08 +0200

    login/register forms

--- EmployeeManagementApp/EmployeeAppFront/src/app/app.module.ts
+++ EmployeeManagementApp/EmployeeAppFront/src/app/app.module.ts
@@ -3,19 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+
 import {FormsModule} from "@angular/forms";
+import {AuthModule} from "./auth/auth.module";
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
+
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    AuthModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
