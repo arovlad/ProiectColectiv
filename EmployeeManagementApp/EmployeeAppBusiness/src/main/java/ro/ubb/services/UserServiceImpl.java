92e423a2251a0264113332454d34ed437ef24aaf
alexgalex98 <73701449+alexgalex98@users.noreply.github.com>
2020-11-23 10:03:25 +0200

    Encryption/Descryption implemented

--- EmployeeManagementApp/EmployeeAppBusiness/src/main/java/ro/ubb/services/UserServiceImpl.java
+++ EmployeeManagementApp/EmployeeAppBusiness/src/main/java/ro/ubb/services/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package ro.ubb.services;
 
 import ro.ubb.exceptions.DbException;
+import ro.ubb.implementations.SHAEncryption;
 import ro.ubb.implementations.UserDaoImpl;
 import ro.ubb.interfaces.UserDao;
 import ro.ubb.interfaces.UserService;
@@ -10,6 +11,7 @@ public class UserServiceImpl implements UserService {
     private static final int WRONG_CREDENTIALS = 1;
     private static final int USER_IS_LOCKED = 2;
     private static final int SUCCESS = 3;
+
 
     public int logIn(String usernameOrEmail, String password) throws DbException {
         UserDao userDao = new UserDaoImpl();
@@ -24,7 +26,7 @@ public class UserServiceImpl implements UserService {
             return WRONG_CREDENTIALS;
         }
 
-        if (user.getPassword().equals(password)) {
+        if (user.getPassword().equals(SHAEncryption.get_SHA1(password))) {
             if (!(userDao.isLocked(usernameOrEmail))) {
                 return SUCCESS;
             } else {
