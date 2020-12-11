c3f6e39907bc059f2ae8d2661dfd053d8fdfcb6c
andrada-code <65818802+andrada-code@users.noreply.github.com>
2020-12-11 15:14:39 +0200

    Login/Dashboard update

--- EmployeeManagementApp/EmployeeAppFront/src/app/dashboard/dashboard-routing.module.ts
+++ EmployeeManagementApp/EmployeeAppFront/src/app/dashboard/dashboard-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { UsersComponent } from './users/users.component';
-import {AddskillComponent} from "./user/addskill/addskill.component";
+import {AddskillComponent} from './user/addskill/addskill.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import { ProfileComponent } from './profile/profile.component';
 import { CreateProjectComponent } from './create-project/create-project.component';
--- EmployeeManagementApp/EmployeeAppFront/src/app/dashboard/dashboard.module.ts
+++ EmployeeManagementApp/EmployeeAppFront/src/app/dashboard/dashboard.module.ts
@@ -6,12 +6,14 @@ import { UsersComponent } from './users/users.component';
 import { AdminComponent } from './admin/admin.component';
 import { UserComponent } from './user/user.component';
 import { AddskillComponent } from './user/addskill/addskill.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
 import { NavbarComponent} from './navbar/navbar.component';
 import { ProfileComponent } from './profile/profile.component';
 import { CreateProjectComponent } from './create-project/create-project.component';
+import {ProjectsComponent} from './users/projects/projects.component';
 
 @NgModule({
+  // tslint:disable-next-line:max-line-length
   declarations: [UsersComponent, AdminComponent, NavbarComponent, ProfileComponent, CreateProjectComponent, ProjectsComponent, AddskillComponent, UserComponent],
   imports: [
     CommonModule,
