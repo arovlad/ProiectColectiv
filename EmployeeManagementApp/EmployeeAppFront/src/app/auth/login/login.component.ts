f36739fe40bfa15e4e0984cf4eec0c1a11e5b393
arovlad <arotariteivlad@gmail.com>
2020-12-12 14:10:24 +0200

    added persistent login info

--- EmployeeManagementApp/EmployeeAppFront/src/app/auth/login/login.component.ts
+++ EmployeeManagementApp/EmployeeAppFront/src/app/auth/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable, Subscription} from 'rxjs';
 import {Router} from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ILoginInformation} from './loginInformation';
@@ -14,67 +13,63 @@ export class LoginComponent implements OnInit {
   username = '';
   password = '';
   message1 = 0;
-  message2 = "";
+  message2 = '';
   message3 = 0;
-message4=0;
-message5=0;
-
+  message4 = 0;
+  message5 = 0;
 
   constructor(private loginService: LoginService, private router: Router) {
   }
 
-  // constructor() {
-  // }
-
   ngOnInit(): void {
   }
- Login(): void {
 
-    if(this.password==''){
-      this.message5=1;
+  Login(): void {
+    if (this.password === ''){
+      this.message5 = 1;
     }
+
+    else {
+      this.message5 = 0;
+    }
+
+    if (this.username === ''){
+     this.message4 = 1;
+    }
+
     else{
-      this.message5=0;
-
+     this.message4 = 0;
     }
-   if(this.username==''){
-     this.message4=1;
-   }
-   else{
-     this.message4=0;
-   }
+
     this.loginService.Login(this.username, this.password).subscribe(
       response => {
-this.message1=0;
-this.message3=0;
-this.message2="";
-
-
-
+        this.message1 = 0;
+        this.message3 = 0;
+        this.message2 = '';
         const loginInfo: ILoginInformation = response.response;
-        if(loginInfo.logInResult == 1 ){
 
-          this.message1=1;
-
-          this.message2=loginInfo.nrRemainigAttempts.toString();
-
-
+        if (loginInfo.logInResult === 1 ){
+          this.message1 = 1;
+          this.message2 = loginInfo.nrRemainigAttempts.toString();
         }
-        else if(loginInfo.logInResult == 2){
-        this.message3=1;
+
+        else if (loginInfo.logInResult === 2){
+        this.message3 = 1;
         }
-        else
-        //if (loginInfo.logInResult === 3)
-          {
-          if (loginInfo.userRoleId === 1){ // employee
-            this.router.navigate(['/dashboard/projects']);
-          }
-          if (loginInfo.userRoleId === 2){ // admin
-            this.router.navigate(['/dashboard/admin']);
-          }
-          if (loginInfo.userRoleId === 3){ // supervisor
-            this.router.navigate(['/dashboard/users']);
-          }
+
+        else {
+            localStorage.setItem('role', String(loginInfo.logInResult));
+            // localStorage.setItem('id', String(loginInfo.userId));
+
+            if (loginInfo.userRoleId === 1){ // employee
+              this.router.navigate(['/dashboard/projects']);
+            }
+            if (loginInfo.userRoleId === 2){ // admin
+              this.router.navigate(['/dashboard/admin']);
+            }
+            if (loginInfo.userRoleId === 3){ // supervisor
+              this.router.navigate(['/dashboard/users']);
+            }
         }
       },
       error => {
