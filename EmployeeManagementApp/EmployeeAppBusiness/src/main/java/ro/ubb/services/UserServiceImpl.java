042aef054d3e6fc218169c1b9d3ff400a6423a8f
DianaBalog <balogdiana99@gmail.com>
2020-12-14 12:39:40 +0200

    Refactor

--- EmployeeManagementApp/EmployeeAppBusiness/src/main/java/ro/ubb/services/UserServiceImpl.java
+++ EmployeeManagementApp/EmployeeAppBusiness/src/main/java/ro/ubb/services/UserServiceImpl.java
@@ -35,21 +35,15 @@ public class UserServiceImpl implements UserService {
             logInResponse.setLogInResult(WRONG_CREDENTIALS);
             logInResponse.setUserRoleId(-1);
             logInResponse.setNrRemainigAttempts(-1);
-<<<<<<< HEAD
-<<<<<<< HEAD
+
             logInResponse.setId(-1);
-=======
-=======
->>>>>>> 28750b2e4e8697e7e575732e89d365403f10c09e
+
 //            try {
 //                User userUpdated = userDao.updateAttempts(usernameOrEmail);
 //            } catch (Exception e) {
 //                e.printStackTrace();
 //            }
-<<<<<<< HEAD
->>>>>>> 28750b2e (added function)
-=======
->>>>>>> 28750b2e4e8697e7e575732e89d365403f10c09e
+
             return logInResponse;
         }
 
