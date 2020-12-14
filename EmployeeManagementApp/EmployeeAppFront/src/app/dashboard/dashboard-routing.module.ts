c7231eb64cd304a4d799d58ab922055fed180c39
andrada-code <65818802+andrada-code@users.noreply.github.com>
2020-12-14 11:27:12 +0200

    update

--- EmployeeManagementApp/EmployeeAppFront/src/app/dashboard/dashboard-routing.module.ts
+++ EmployeeManagementApp/EmployeeAppFront/src/app/dashboard/dashboard-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { UsersComponent } from './users/users.component';
-import {CrudTechnologyAreaComponent} from "./admin/crud-technology-area/crud-technology-area.component";
+import {CrudTechnologyAreaComponent} from './admin/crud-technology-area/crud-technology-area.component';
 import {AddskillComponent} from './user/addskill/addskill.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -14,14 +14,15 @@ const routes: Routes = [
   { path: 'dashboard/admin', component: AdminComponent },
   { path: 'dashboard/users', component: UsersComponent},
   { path: 'dashboard/user/addskill', component: AddskillComponent},
-  {path:'dashboard/admin/CrudTechnologyArea',component:CrudTechnologyAreaComponent }
+  {path: 'dashboard/admin/CrudTechnologyArea', component: CrudTechnologyAreaComponent },
   { path: 'dashboard/navbar', component: NavbarComponent},
   {path: 'dashboard/profile', component: ProfileComponent},
   { path: 'dashboard/create-project', component: CreateProjectComponent},
   { path: 'dashboard/users', component: UsersComponent},
   { path: 'dashboard/my-profile', component: MyProfileComponent},
   {path: 'dashboard/add-skills', component: AddSkillsComponent},
-  { path: 'dashboard/user/addskill', component: AddskillComponent}
+  { path: 'dashboard/user/addskill', component: AddskillComponent},
+
 ];
 
 @NgModule({
